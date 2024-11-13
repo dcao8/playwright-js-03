e433c66a30aada50f71e82115161f1940cd85e23
dcao8 <daych93@gmail.com>
2024-11-13 23:03:15 +0700

    [ID-07] Completed verify Dropdown

---
+++ data/elements/drop-down-test-data.js
@@ -0,0 +1,6 @@
+export const dropDownTestData = [
+    '1st menu item',
+    '2nd menu item',
+    '3rd menu item',
+    '4rd menu item'
+]
---
+++ tests/elements/drop-down.spec.js
@@ -0,0 +1,14 @@
+const { test, expect } = require('@playwright/test');
+const { dropDownTestData } = require('../../data/elements/drop-down-test-data');
+
+dropDownTestData.forEach(value => {
+    test(`Verify Radio button with value '${value}'`, async ({ page }) => {
+        await page.goto('https://test-with-me-app.vercel.app/learning/web-elements/elements/dropdown');
+        let btnEllipsisXpath="//button[.//span[@aria-label='ellipsis']]";
+        let dropDownItemXpath = `//li[.//span[normalize-space(text())='${value}']]`;
+        let valueLabelXpath = `//div[./span[normalize-space(text())='${value}']]`;
+        await page.locator(btnEllipsisXpath).hover();
+        await page.locator(dropDownItemXpath).click();
+        await expect(page.locator(valueLabelXpath)).toBeVisible();
+    });
+})
