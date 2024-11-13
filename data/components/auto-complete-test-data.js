e903df2f145c35928a936978adb4d99533c8f624
dcao8 <daych93@gmail.com>
2024-11-13 23:03:43 +0700

    [ID-08] Completed verify AutoComplete

---
+++ data/components/auto-complete-test-data.js
@@ -0,0 +1,19 @@
+export const validKeyword = [
+    {
+        keyword: "b",
+        itemOption: "Burns Bay Road"
+    },
+    {
+        keyword: "d",
+        itemOption: "Downing Street"
+    },
+    {
+        keyword: "w",
+        itemOption: "Wall Street"
+    }
+]
+
+export const invalidKeyword = [
+    "   ",
+    "day"
+]
---
+++ tests/components/auto-complete.spec.js
@@ -0,0 +1,28 @@
+const { test, expect } = require('@playwright/test');
+const { validKeyword, invalidKeyword } = require('../../data/components/auto-complete-test-data');
+
+let inputXpath="//input";
+
+async function fillThenSelect(page, keyword, itemOption) {
+    let itemOptionXpath=`//div[@aria-selected='false']//div[normalize-space(text())='${itemOption}']`;
+    await page.locator(inputXpath).fill(keyword);
+    await page.locator(itemOptionXpath).click();
+}
+
+validKeyword.forEach(({ keyword, itemOption }) => {
+    test(`Verify selecting ${itemOption} with keyword ${keyword}`, async ({ page }) => {
+        await page.goto('https://test-with-me-app.vercel.app/learning/web-elements/components/auto-complete');
+        await fillThenSelect(page, keyword, itemOption);
+        let valueLabelXpath = "//div[normalize-space(text())='Value:']";
+        await expect(page.locator(valueLabelXpath)).toHaveText(`Value: ${itemOption} was selected!`);
+    });
+})
+
+invalidKeyword.forEach(keyword => {
+    test(`Verify no item display when searching with keyword ${keyword}`, async ({ page }) => {
+        await page.goto('https://test-with-me-app.vercel.app/learning/web-elements/components/auto-complete');
+        await page.locator(inputXpath).fill(keyword);
+        let listItemsXpath = "//div[@class='rc-virtual-list']";
+        await expect(page.locator(listItemsXpath)).toBeHidden();
+    });
+}) 
