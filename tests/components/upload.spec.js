b2e81889d0c36b89ea4d3b9b2f25f7704ec23c16
dcao8 <daych93@gmail.com>
2024-11-27 13:07:31 +0700

    [ID-22] Optimize verify Upload

--- tests/components/upload.spec.js
+++ tests/components/upload.spec.js
@@ -1,15 +1,17 @@
 const { test, expect } = require('@playwright/test');
 const { uploadTestData } = require('../../data/components/upload-test-data');
+const path = require('path');
 
 let urlUpload = 'https://test-with-me-app.vercel.app/learning/web-elements/components/upload';
-uploadTestData.forEach(({ testCase, path, uploadFile }) => {
+uploadTestData.forEach(({ testCase, pathFolder, uploadFile }) => {
     test(`Verify ${testCase}`, async ({ page }) => {
         await page.goto(urlUpload);
         let inputClickToUploadXpath = "//button[.//text()[normalize-space()='Click to Upload']]/preceding-sibling::input[@type='file']";
         for (let i = 0; i < uploadFile.length; i++) {
-            await page.locator(inputClickToUploadXpath).setInputFiles(path + uploadFile[i]);
+            let pathToUploadFile = path.join(process.cwd(),`${pathFolder}/${uploadFile[i]}`);
+            await page.locator(inputClickToUploadXpath).setInputFiles(pathToUploadFile);
         }
         let expectedResult = "//span[contains(concat(' ',@class,' '), ' ant-upload-list-item-name ')]"
         await expect(page.locator(expectedResult)).toHaveText(uploadFile);
-    })
-})
+    });
+});
