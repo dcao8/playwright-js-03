e29f1b99701d45a110a144bfd7b2b4c98c4b8bd6
dcao8 <daych93@gmail.com>
2024-11-29 15:53:44 +0700

    [ID-25] Complete verify Table

--- data/components/table-test-data.js
+++ data/components/table-test-data.js
@@ -1,23 +1,92 @@
 export const tableTestData = [
     {
-        name: "Jim Brown",
-        address: "London No. 8 Lake Park",
-        age: "32",
-        tags: ["COOL", "TEACHER"],
-        action: "Invite"
+        "Name": "John Brown",
+        "Age": "32",
+        "Address": "New York No. 1 Lake Park",
+        "Tags": "NICEDEVELOPER"
     },
     {
-        name: "Jim Black",
-        address: "London No. 10 Lake Park",
-        age: "32",
-        tags: ["COOL", "TEACHER"],
-        action: "Invite"
+        "Name": "Jim Green",
+        "Age": "42",
+        "Address": "London No. 1 Lake Park",
+        "Tags": "LOSER"
     },
     {
-        name: "Jim Black1",
-        address: "London No. 10 Lake Park",
-        age: "32",
-        tags: ["COOL", "TEACHER"],
-        action: "Invite"
+        "Name": "Joe Black",
+        "Age": "32",
+        "Address": "Sidney No. 1 Lake Park",
+        "Tags": "COOLTEACHER"
+    },
+    {
+        "Name": "Jim Red",
+        "Age": "32",
+        "Address": "London No. 2 Lake Park",
+        "Tags": "COOLTEACHER"
+    },
+    {
+        "Name": "Jim Blue",
+        "Age": "32",
+        "Address": "London No. 3 Lake Park",
+        "Tags": "COOLTEACHER"
+    },
+    {
+        "Name": "Jim Yellow",
+        "Age": "32",
+        "Address": "London No. 4 Lake Park",
+        "Tags": "COOLTEACHER"
+    },
+    {
+        "Name": "Jim Pink",
+        "Age": "32",
+        "Address": "London No. 5 Lake Park",
+        "Tags": "COOLTEACHER"
+    },
+    {
+        "Name": "Jim Orange",
+        "Age": "32",
+        "Address": "London No. 6 Lake Park",
+        "Tags": "COOLTEACHER"
+    },
+    {
+        "Name": "Jim Purple",
+        "Age": "32",
+        "Address": "London No. 7 Lake Park",
+        "Tags": "COOLTEACHER"
+    },
+    {
+        "Name": "Jim Brown",
+        "Age": "32",
+        "Address": "London No. 8 Lake Park",
+        "Tags": "COOLTEACHER"
+    },
+    {
+        "Name": "Jim Green",
+        "Age": "32",
+        "Address": "London No. 9 Lake Park",
+        "Tags": "COOLTEACHER"
+    },
+    {
+        "Name": "Jim Black",
+        "Age": "32",
+        "Address": "London No. 10 Lake Park",
+        "Tags": "COOLTEACHER"
+    },
+    {
+        "Name": "Jim Red",
+        "Age": "32",
+        "Address": "London No. 11 Lake Park",
+        "Tags": "COOLTEACHER"
+    },
+    {
+        "Name": "Jim Blue",
+        "Age": "32",
+        "Address": "London No. 12 Lake Park",
+        "Tags": "COOLTEACHER"
+    },
+    {
+        "Name": "Jim Yellow",
+        "Age": "32",
+        "Address": "London No. 13 Lake Park",
+        "Tags": "COOLTEACHER"
     }
 ]
--- tests/components/table.spec.js
+++ tests/components/table.spec.js
@@ -2,67 +2,41 @@ const { test, expect } = require('@playwright/test');
 const { tableTestData } = require('../../data/components/table-test-data');
 
 let urlTable = 'https://test-with-me-app.vercel.app/learning/web-elements/components/table';
-let indexNameColumn;
-let indexAgeColumn;
-let indexAddressColumn;
-let indexTagsColumn;
-let indexActionColumn;
 
-tableTestData.forEach(({ name, address, age, tags, action }) => {
-    test(`Verify table with name: '${name}'`, async ({ page }) => {
-        await page.goto(urlTable);
-        let headerXpath = "//thead[contains(concat(' ',@class,' '),' ant-table-thead ')]//th[@scope='col']";
-        await getColumnsIndex(headerXpath, page);
-        let expectedRowXpath = `//tr[.//text()[normalize-space()='${name}']]`;
-        await findRowByName(expectedRowXpath, page);
-        let expectedRow = page.locator(expectedRowXpath);
-        expect.soft(await getAddress(expectedRow)).toBe(address);
-        expect.soft(await getAge(expectedRow)).toBe(age);
-        expect.soft(await getTags(expectedRow)).toEqual(expect.arrayContaining(tags));
-        await clickActionButton(expectedRow, action);
-    });
-})
-
-async function findRowByName(expectedRowXpath, page) {
-    while (await page.locator(expectedRowXpath).count() < 1) {
-        try {
-            let btnNextPageXpath = "//li[.//span[@aria-label='right'] and @aria-disabled='false']";
-            await page.locator(btnNextPageXpath).click();
-        } catch {
-            throw new Error(`Could not found element with xPath: ${expectedRowXpath}`);
+test(`Verify table`, async ({ page }) => {
+    await page.goto(urlTable);
+    await page.waitForTimeout(3000);
+    let headerXpath = "(//div[./span[normalize-space(text())='Table']]/following::table)[1]//thead//th";
+    let heardersName = await page.locator(headerXpath).allTextContents();
+    let rowXpath = "(//div[./span[normalize-space(text())='Table']]/following::table)[1]//tbody//tr";
+    let rows;
+    let actualData = [];
+    let isBtnNextIsDisabled;
+    let columnNeeded = ['Name', 'Age', 'Address', 'Tags'];
+    do {
+        rows = await page.locator(rowXpath).all();
+        for (let row of rows) {
+            let rowData = {};
+            for (let column of columnNeeded) {
+                rowData[column] = await getCellData(heardersName, column, row)
+            }
+            actualData.push(rowData);
         }
-    }
+        let btnNextPageXpath = "(//div[./span[normalize-space(text())='Table']]/following::div[contains(concat(' ',@class,' '),' ant-table-wrapper ')])[1]//li[@title='Next Page']";
+        let isDisabled = await page.locator(btnNextPageXpath).getAttribute('aria-disabled');
+        isBtnNextIsDisabled = isDisabled == 'false';
+        if (isBtnNextIsDisabled) {
+            await page.locator(btnNextPageXpath).click();
+        }
+    } while (isBtnNextIsDisabled);
+    expect(actualData.length).toEqual(tableTestData.length);
+    expect(actualData).toEqual(expect.arrayContaining(tableTestData));
+    expect(tableTestData).toEqual(expect.arrayContaining(actualData));
+});
+
+async function getCellData(heardersName, columnName, row) {
+    let indexName = heardersName.indexOf(columnName);
+    let cells = await row.locator(`//td[${indexName + 1}]`).textContent();
+    return cells.trim();
 }
 
-async function getAddress(row) {
-    let cellsInRow = await row.locator("td");
-    return await cellsInRow.nth(indexAddressColumn).textContent();
-}
-
-async function getAge(row) {
-    let cellsInRow = row.locator("td");
-    return await cellsInRow.nth(indexAgeColumn).textContent();
-}
-
-async function getTags(row) {
-    let cellsInRow = await row.locator("//td");
-    let tags = [];
-    for (let tag of await cellsInRow.nth(indexTagsColumn).locator("//span").all()) {
-        tags.push(await tag.textContent());
-    }
-    return tags;
-}
-
-async function clickActionButton(row, action) {
-    let cellsInRow = await row.locator("//td");
-    await cellsInRow.nth(indexActionColumn).locator(`//a[contains(text(),'${action}')]`).click();
-}
-
-async function getColumnsIndex(headerXpath, page) {
-    let allColumnNames = await page.locator(headerXpath).allTextContents();
-    indexNameColumn = allColumnNames.indexOf("Name");
-    indexAgeColumn = allColumnNames.indexOf("Age");
-    indexAddressColumn = allColumnNames.indexOf("Address");
-    indexTagsColumn = allColumnNames.indexOf("Tags");
-    indexActionColumn = allColumnNames.indexOf("Action");
-}
